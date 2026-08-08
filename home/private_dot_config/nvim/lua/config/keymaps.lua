d881ccb3009f2188244d3af77938f7f10141bc0c
wigner <240170694+wignerStan@users.noreply.github.com>
2026-08-08 08:06:48 +0800

    fix(nvim): keep deletes out of clipboard

--- home/private_dot_config/nvim/lua/config/keymaps.lua
+++ home/private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Keep deletes out of the unnamed/system clipboard. Explicit yanks still use
+-- `unnamedplus`, as configured in lua/plugins/clipboard.lua.
+vim.keymap.set({ "n", "x" }, "d", '"_d', { desc = "Delete without copying" })
